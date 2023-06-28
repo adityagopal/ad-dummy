ce61178496544a28371946fbd677a72cc78c3fc0
Aditya Gopal <aditya.gopal@topcoder.com>
2023-06-29 04:30:55 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+y
